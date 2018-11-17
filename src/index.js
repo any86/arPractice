a0a98ccf66ca3d65bbc589acbf2d05762c70bf3b
宁宁 <383514580@qq.com>
2018-11-17 17:21:24 +0800

    99

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-
-
-
 import {
     ARCameraParam,
     ARController
 } from 'jsartoolkit5';
 var video = ARController.getUserMedia({
-    
+
     maxARVideoSize: 320, // do AR processing on scaled down video of this size
 
     facing: "environment",
