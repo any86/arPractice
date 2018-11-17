29eba7d1243e3684783f7934453aeee68ab881a9
宁宁 <383514580@qq.com>
2018-11-17 15:03:44 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
+
+
+
 import {
     ARCameraParam,
     ARController
 } from 'jsartoolkit5';
-var param = new ARCameraParam();
-param.onload = function() {
-    console.log(321);
-    var img = document.getElementById('test-img');
-    var ar = new ARController(img.width, img.height, param);
-    // Set pattern detection mode to detect both pattern markers and barcode markers.
-    // This is more error-prone than detecting only pattern markers (default) or only barcode markers.
-    //
-    // For barcode markers, use artoolkit.AR_MATRIX_CODE_DETECTION
-    // For pattern markers, use artoolkit.AR_TEMPLATE_MATCHING_COLOR
-    //
-    ar.setPatternDetectionMode(artoolkit.AR_TEMPLATE_MATCHING_COLOR_AND_MATRIX);
+var video = ARController.getUserMedia({
+    
+    maxARVideoSize: 320, // do AR processing on scaled down video of this size
 
-    ar.addEventListener('markerNum', function(ev) {
-        console.log('got markers', markerNum);
-    });
+    facing: "environment",
 
-    ar.addEventListener('getMarker', function(ev) {
-        console.log('found marker?', ev);
-    });
-
-    ar.loadMarker('Data/patt.hiro', function(marker) {
-        console.log('loaded marker', marker);
-        ar.process(img);
-    });
-}
+    onSuccess: function(video) {
+        console.log('got video', video);
+    }
+})
