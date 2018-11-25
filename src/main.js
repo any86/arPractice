a14adfeb832d3b3a39652b8ffdf950d42df3c865
宁宁 <383514580@qq.com>
2018-11-25 13:53:25 +0800

    123

--- src/main.js
+++ src/main.js
@@ -7,24 +7,58 @@ import {
 import createBox2d from './createBox2d';
 import getCameraVideo from './getCameraVideo';
 
-(async()=>{
-    alert(321)
-})();
+
+// async function inin(){
+//     alert(33211)
+// }
+// inin();
 
 
-let img = document.getElementById('img');
-var arController = new ARController(img, 'Data/camera_para.dat');
-arController.onload = function() {
-    arController.setPatternDetectionMode(artoolkit.AR_TEMPLATE_MATCHING_COLOR);
-    arController.addEventListener('getMarker', function(ev) {
-        console.log(ev);
-        console.log(ev.target.videoWidth, ev.target.videoHeight, ev.data.marker.vertex);
-        const vertex = ev.data.marker.vertex;
-        createBox2d({
-            width: ev.target.videoWidth,
-            height: ev.target.videoHeight,
-            vertex: ev.data.marker.vertex
+const video = document.getElementById('video');
+getCameraVideo().then(stream => {
+    const url = window.URL.createObjectURL(stream);
+    video.src = url;
+
+
+    let img = document.getElementById('img');
+    var arController = new ARController(video, 'Data/camera_para.dat');
+    arController.onload = function() {
+        arController.setPatternDetectionMode(artoolkit.AR_TEMPLATE_MATCHING_COLOR);
+        arController.addEventListener('getMarker', function(ev) {
+            console.log(ev);
+            console.log(ev.target.videoWidth, ev.target.videoHeight, ev.data.marker.vertex);
+            const vertex = ev.data.marker.vertex;
+            createBox2d({
+                width: ev.target.videoWidth,
+                height: ev.target.videoHeight,
+                vertex: ev.data.marker.vertex
+            });
         });
-    });
-    arController.process();
-};
+        arController.process(video);
+    };
+
+
+});
+document.addEventListener('click',ev=>{
+    video.play();
+})
+
+
+
+
+// let img = document.getElementById('img');
+// var arController = new ARController(img, 'Data/camera_para.dat');
+// arController.onload = function() {
+//     arController.setPatternDetectionMode(artoolkit.AR_TEMPLATE_MATCHING_COLOR);
+//     arController.addEventListener('getMarker', function(ev) {
+//         console.log(ev);
+//         console.log(ev.target.videoWidth, ev.target.videoHeight, ev.data.marker.vertex);
+//         const vertex = ev.data.marker.vertex;
+//         createBox2d({
+//             width: ev.target.videoWidth,
+//             height: ev.target.videoHeight,
+//             vertex: ev.data.marker.vertex
+//         });
+//     });
+//     arController.process();
+// };
